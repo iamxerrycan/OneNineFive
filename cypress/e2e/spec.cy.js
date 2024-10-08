bb7118a0b1cbc0a0e751974beec5568b556dd706
rajshish333 <rajshish333@gmail.com>
2024-10-08 14:00:23 +0530

    cypress working fine

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,12 @@
-describe('template spec', () => {
+/* eslint-disable no-undef */
+describe('main page visit', () => {
   it('passes', () => {
     // eslint-disable-next-line no-undef
     cy.visit('http://localhost:3000/')
+    
+    cy.get("a[href='/about']").click()
+    .then(()=>{
+      cy.url().should("include", '/about')
+    })
   })
 })
